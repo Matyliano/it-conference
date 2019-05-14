c6324cab1a3c2059a4a6732b8b11729c278341ff
matyliano <joanna.boszko@gmail.com>
2019-05-14 11:59:35 +0200

    add getters in Lecture class

--- src/main/java/sii/matyliano/spring/entity/Lecture.java
+++ src/main/java/sii/matyliano/spring/entity/Lecture.java
@@ -31,4 +31,20 @@ public class Lecture {
         this.date = date;
         this.time = time;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public LocalTime getTime() {
+        return time;
+    }
 }
